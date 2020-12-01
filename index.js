51337fa4ab2c9505d554c943382e70384bf722bb
shashankgs99 <shashankgs99@gmail.com>
2020-12-02 02:35:00 +0530

    vcc and ground

--- index.js
+++ index.js
@@ -59,67 +59,86 @@ function checkPower() {
       return out;
     }
 
-    var andInputs =[];
+    var andInputs = [];
     for (var j = 1; j < 9; j++) {
       var andGate = "#and" + j;
       document.querySelector(andGate).addEventListener('change', checking);
       andInputs.push(checking(document.querySelector(andGate)));
     }
 
-    var orInputs =[];
+    var orInputs = [];
     for (var j = 1; j < 9; j++) {
       var orGate = "#or" + j;
       document.querySelector(orGate).addEventListener('change', checking);
       orInputs.push(checking(document.querySelector(orGate)));
     }
 
-    var nandInputs =[];
+    var nandInputs = [];
     for (var j = 1; j < 9; j++) {
       var nandGate = "#nand" + j;
       document.querySelector(nandGate).addEventListener('change', checking);
       nandInputs.push(checking(document.querySelector(nandGate)));
     }
 
-    var norInputs =[];
+    var norInputs = [];
     for (var j = 1; j < 9; j++) {
       var norGate = "#nor" + j;
       document.querySelector(norGate).addEventListener('change', checking);
       norInputs.push(checking(document.querySelector(norGate)));
     }
 
-    var xorInputs =[];
+    var xorInputs = [];
     for (var j = 1; j < 9; j++) {
       var xorGate = "#xor" + j;
       document.querySelector(xorGate).addEventListener('change', checking);
       xorInputs.push(checking(document.querySelector(xorGate)));
     }
 
-    and(andInputs[0], andInputs[1], 0);
-    and(andInputs[2], andInputs[3], 1);
-    and(andInputs[4], andInputs[5], 3);
-    and(andInputs[6], andInputs[7], 2);
+    var main = ["And", "Or", "Nand", "Nor", "Xor"];
+    var vccVal = [];
+    var groundVal = [];
+    for (var k = 0; k < 5; k++) {
+      var grounds = ".ground" + main[k];
+      console.log(grounds);
+      var vccs = ".vcc" + main[k];
+      groundVal.push(checking(document.querySelector(grounds)));
+      vccVal.push(checking(document.querySelector(vccs)));
+    }
+    console.log(groundVal);
+    console.log(vccVal);
 
-    or(orInputs[0], orInputs[1], 4);
-    or(orInputs[2], orInputs[3], 5);
-    or(orInputs[4], orInputs[5], 7);
-    or(orInputs[6], orInputs[7], 6);
+    if ((vccVal[0] == 1) && (groundVal[0] == 1)) {
+      and(andInputs[0], andInputs[1], 0);
+      and(andInputs[2], andInputs[3], 1);
+      and(andInputs[4], andInputs[5], 3);
+      and(andInputs[6], andInputs[7], 2);
+    }
+    if ((vccVal[1] == 1) && (groundVal[1] == 1)) {
+      or(orInputs[0], orInputs[1], 4);
+      or(orInputs[2], orInputs[3], 5);
+      or(orInputs[4], orInputs[5], 7);
+      or(orInputs[6], orInputs[7], 6);
+    }
+    if ((vccVal[2] == 1) && (groundVal[2] == 1)) {
+      nand(nandInputs[0], nandInputs[1], 8);
+      nand(nandInputs[2], nandInputs[3], 9);
+      nand(nandInputs[4], nandInputs[5], 11);
+      nand(nandInputs[6], nandInputs[7], 10);
+    }
+    if ((vccVal[3] == 1) && (groundVal[3] == 1)) {
+      nor(norInputs[0], norInputs[1], 12);
+      nor(norInputs[2], norInputs[3], 13);
+      nor(norInputs[4], norInputs[5], 15);
+      nor(norInputs[6], norInputs[7], 14);
+    }
+    if ((vccVal[4] == 1) && (groundVal[4] == 1)) {
+      xor(xorInputs[0], xorInputs[1], 16);
+      xor(xorInputs[2], xorInputs[3], 17);
+      xor(xorInputs[4], xorInputs[5], 19);
+      xor(xorInputs[6], xorInputs[7], 18);
+    }
 
-    nand(nandInputs[0], nandInputs[1], 8);
-    nand(nandInputs[2], nandInputs[3], 9);
-    nand(nandInputs[4], nandInputs[5], 11);
-    nand(nandInputs[6], nandInputs[7], 10);
-
-    nor(norInputs[0], norInputs[1], 12);
-    nor(norInputs[2], norInputs[3], 13);
-    nor(norInputs[4], norInputs[5], 15);
-    nor(norInputs[6], norInputs[7], 14);
-
-    xor(xorInputs[0], xorInputs[1], 16);
-    xor(xorInputs[2], xorInputs[3], 17);
-    xor(xorInputs[4], xorInputs[5], 19);
-    xor(xorInputs[6], xorInputs[7], 18);
   } else {
-
     var numButtons = document.querySelectorAll(".off").length;
     for (var i = 0; i < numButtons; i++) {
       document.querySelectorAll(".off")[i].style.backgroundColor = "#efefef";
